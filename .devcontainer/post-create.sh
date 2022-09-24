1e2386d7ea237e3e69fca3cefeae85c081d7647b
Shahrad Rezaei <shahrad@rezaei.io>
2022-09-24 05:06:03 +0000

    chore: install the PyEZ Python library
    Install the Junos PyEZ, a "Python library that enables you to manage and
    automate devices running Junos OS".
    
    This commit also updates the devcontainer post-create script to install
    the project's Python libraries at container creation.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,6 +4,7 @@ pre-commit install --install-hooks
 pre-commit install --install-hooks --hook-type commit-msg
 
 pip install codespell
+pip install -r requirements.txt
 
 npm install -g commitizen
 npm install -g cz-conventional-changelog

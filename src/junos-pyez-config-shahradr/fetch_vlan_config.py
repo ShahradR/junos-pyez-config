873066ee5e3a6ee98a396705fbbe1c68986d8dff
Shahrad Rezaei <shahrad@rezaei.io>
2022-11-28 06:52:58 +0000

    feat: manually instrument fetch_vlan_config
    Manually instrument the fetch_vlan_config module using OpenTelemetry to
    trace the execution of the connect_to_device function.

--- src/junos-pyez-config-shahradr/fetch_vlan_config.py
+++ src/junos-pyez-config-shahradr/fetch_vlan_config.py
@@ -11,6 +11,7 @@ from connection.connection_info import (
 from jnpr.junos import Device
 from jnpr.junos.exception import ConnectError
 from lxml import etree
+from opentelemetry import trace
 
 VLAN_FILTER = "<configuration><vlans/></configuration>"
 
@@ -20,22 +21,27 @@ def connect_to_device(device_config: NetworkApplianceConnectionInfo):
     Connect to a Juniper Networks appliance using the provided configuration, and retrieve the
     VLAN configuration.
     """
-    try:
-        with Device(
-            host=device_config.hostname,
-            user=device_config.username,
-            password=device_config.password,
-        ) as dev:
-            print(
-                etree.tostring(
-                    dev.rpc.get_config(filter_xml=filter),
-                    encoding="unicode",
-                    pretty_print=True,
+
+    # Creates a tracer from the global tracer provider
+    tracer = trace.get_tracer(__name__)
+
+    with tracer.start_as_current_span("connect_to_device") as span:
+        try:
+            with Device(
+                host=device_config.hostname,
+                user=device_config.username,
+                password=device_config.password,
+            ) as dev:
+                print(
+                    etree.tostring(
+                        dev.rpc.get_config(filter_xml=filter),
+                        encoding="unicode",
+                        pretty_print=True,
+                    )
                 )
-            )
-    except ConnectError as err:
-        print(f"Cannot connect to device: {err}")
-        sys.exit(1)
+        except ConnectError as err:
+            print(f"Cannot connect to device: {err}")
+            sys.exit(1)
 
 
 connect_to_device(ConnectionInfoManager().get_configuration())

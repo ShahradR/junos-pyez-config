f9fca0e8f3d69d7f5d3ff28dbc2033196284b2c3
Shahrad Rezaei <shahrad@rezaei.io>
2022-12-31 04:49:42 +0000

    chore: instrument the connection_info module
    Instrument the connection_info module to create spans under the parent
    trace.
    
    To support this change, the fetch_vlan_config module was also updated to
    create a parent trace when starting the application, rather than having
    it scoped only to its connect_to_device() function.

--- src/junos-pyez-config-shahradr/fetch_vlan_config.py
+++ src/junos-pyez-config-shahradr/fetch_vlan_config.py
@@ -22,26 +22,26 @@ def connect_to_device(device_config: NetworkApplianceConnectionInfo):
     VLAN configuration.
     """
 
-    # Creates a tracer from the global tracer provider
-    tracer = trace.get_tracer(__name__)
-
-    with tracer.start_as_current_span("connect_to_device") as span:
-        try:
-            with Device(
-                host=device_config.hostname,
-                user=device_config.username,
-                password=device_config.password,
-            ) as dev:
-                print(
-                    etree.tostring(
-                        dev.rpc.get_config(filter_xml=filter),
-                        encoding="unicode",
-                        pretty_print=True,
-                    )
+    try:
+        with Device(
+            host=device_config.hostname,
+            user=device_config.username,
+            password=device_config.password,
+        ) as dev:
+            print(
+                etree.tostring(
+                    dev.rpc.get_config(filter_xml=filter),
+                    encoding="unicode",
+                    pretty_print=True,
                 )
-        except ConnectError as err:
-            print(f"Cannot connect to device: {err}")
-            sys.exit(1)
+            )
+    except ConnectError as err:
+        print(f"Cannot connect to device: {err}")
+        sys.exit(1)
 
 
-connect_to_device(ConnectionInfoManager().get_configuration())
+# Creates a tracer from the global tracer provider
+tracer = trace.get_tracer(__name__)
+
+with tracer.start_as_current_span("connect_to_device") as span:
+    connect_to_device(ConnectionInfoManager().get_configuration())
